8f775e61e5566d87116af4481129f5d1b39615ac
Lucas Vieira <vieiralucas4@gmail.com>
2017-03-31 17:51:34 -0300

    Fix Rott.prod.js

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 
-const Root ({ store }) => {
+const Root = ({ store, children }) => (
   <Provider store={store}>
     {children}
   </Provider>
